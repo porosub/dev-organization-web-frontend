0e8509f6cc959473b38bce5f6e310fce2968a5d6
fikrimus35 <mustofa13.fikri@gmail.com>
2021-12-28 06:30:16 +0700

    Filter needed data

--- server/services/scraper.js
+++ server/services/scraper.js
@@ -7,14 +7,19 @@ export const scrapePost = async (req, res) => {
         const response = await axios.get('https://www.instagram.com/porosfilkom/');
         const html = response.data;
         const regex = /window\._sharedData\s?=\s?({.+);\s?<\/script>/
-        const data = html.match(regex)[1]
-        const json = JSON.parse(data)
+        const rawData = html.match(regex)[1]
+        const jsonData = JSON.parse(rawData)
+        const timelines = jsonData.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges
+        const result = timelines.map(timeline => ({
+            display_url: timeline.node.display_url,
+            caption: timeline.node.edge_media_to_caption.edges[0].node.text
+        }))
         
         // monggo diambil data postnya dari json tsb. datanya ada di key edge_owner_to_timeline_media
 
         return res.status(200).send({
             message: 'success',
-            data: json
+            data: result
         })
     } catch (err) {
         return res.status(500).send({
